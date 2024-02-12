aaeb09f8ec746e16168d1abc5518c46948fdbf46
NickNoss <nnossenko@gmail.com>
2024-02-12 12:04:02 +0200

    Started anew

--- Osa6_Anecdotes/src/App.jsx
+++ Osa6_Anecdotes/src/App.jsx
@@ -1,27 +1,19 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { useQuery } from '@tanstack/react-query'
-import { getAnecdotes } from './services/requests'
 
 const App = () => {
-
-  const result = useQuery({
-    queryKey: ['anecdotes'],
-    queryFn: getAnecdotes
-  })
-  console.log(JSON.parse(JSON.stringify(result)))
 
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  if ( result.isLoading ) {
-    return <div>loading data...</div>
-  } else if ( result.isError ) {
-    return <div>anecdote service not available due to problems in server</div>
-  }
-
-  const anecdotes = result.data
+  const anecdotes = [
+    {
+      "content": "If it hurts, do it more often",
+      "id": "47145",
+      "votes": 0
+    },
+  ]
 
   return (
     <div>
