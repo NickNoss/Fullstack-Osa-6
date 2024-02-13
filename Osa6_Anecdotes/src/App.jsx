0fc70c339d67c1092d5fd59102e68b545d6c9218
NickNoss <nnossenko@gmail.com>
2024-02-13 12:19:32 +0200

    Tehty 6.21-6.22

--- Osa6_Anecdotes/src/App.jsx
+++ Osa6_Anecdotes/src/App.jsx
@@ -1,16 +1,23 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { useQuery, useQueryClient } from '@tanstack/react-query'
-import { getAnecdotes } from './requests'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { getAnecdotes, voteAnecdote } from './requests'
 
 
 const App = () => {
 
   const queryClient = useQueryClient()
 
+  const voteAnecdoteMutation = useMutation({
+    mutationFn: voteAnecdote
+    ,
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ['anecdotes']})
+    }
+  })
 
   const handleVote = (anecdote) => {
-    console.log('vote')
+    voteAnecdoteMutation.mutate({...anecdote, votes: anecdote.votes + 1})
   }
 
   const result = useQuery({
